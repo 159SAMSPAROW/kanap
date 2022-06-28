df400d39e2ebfa15d776b90e7496be0d1b96c423
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2022-06-28 14:46:41 +0200

    p5 start

---
+++ product.js
@@ -0,0 +1,16 @@
+const kanap = fetch("http://localhost:3000/api/products/id");
+
+kanap.then(function(res) {
+
+    if (res.ok) {
+        return res.json();
+    }
+})
+
+.then(function(value) {
+    console.log(value);
+})
+
+.catch(function(err) {
+    console.log(erreur)
+});
